e3001af5e05d6e913b61d8a91fe409d459a4aeeb
albertocmr <cam.ortiz.al@gmail.com>
2024-03-25 12:37:19 +0100

    Agregamos el guion de entrega y los codigos que faltaban

---
+++ Algoritmica/Practica1/codigos/shellSort.cpp
@@ -0,0 +1,102 @@
+#include <cstdlib> // Para usar srand y rand
+#include <chrono>
+#include <cstring> // Para usar memcpy
+#include <iostream>
+#include <fstream> // Para usar ficheros
+using namespace std; 
+
+/* function to sort arr using shellSort */
+int shellSort(int arr[], int n) 
+{ 
+	// Start with a big gap, then reduce the gap 
+	for (int gap = n/2; gap > 0; gap /= 2) 
+	{ 
+		// Do a gapped insertion sort for this gap size. 
+		// The first gap elements a[0..gap-1] are already in gapped order 
+		// keep adding one more element until the entire array is 
+		// gap sorted 
+		for (int i = gap; i < n; i += 1) 
+		{ 
+			// add a[i] to the elements that have been gap sorted 
+			// save a[i] in temp and make a hole at position i 
+			int temp = arr[i]; 
+
+			// shift earlier gap-sorted elements up until the correct 
+			// location for a[i] is found 
+			int j;			 
+			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+				arr[j] = arr[j - gap]; 
+			
+			// put temp (the original a[i]) in its correct location 
+			arr[j] = temp; 
+		} 
+	} 
+	return 0; 
+}
+
+
+
+int main(int argc, char *argv[]) {
+	
+	int *v;
+	int n, i, argumento;
+    chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
+	unsigned long int semilla;
+	ofstream fsalida;
+	
+	if (argc <= 3) {
+		cerr<<"\nError: El programa se debe ejecutar de la siguiente forma.\n\n";
+		cerr<<argv[0]<<" NombreFicheroSalida Semilla tamCaso1 tamCaso2 ... tamCasoN\n\n";
+		return 0;
+	}
+	
+	// Abrimos fichero de salida
+	fsalida.open(argv[1]);
+	if (!fsalida.is_open()) {
+		cerr<<"Error: No se pudo abrir fichero para escritura "<<argv[1]<<"\n\n";
+		return 0;
+	}
+	
+	// Inicializamos generador de no. aleatorios
+	semilla= atoi(argv[2]);
+	srand(semilla);
+	
+	// Pasamos por cada tamaÒo de caso
+	for (argumento= 3; argumento<argc; argumento++) {
+		
+		// Cogemos el tamanio del caso
+		n= atoi(argv[argumento]);
+		
+		// Reservamos memoria para el vector
+		v= new int[n];
+		
+		// Generamos vector aleatorio de prueba, con componentes entre 0 y n-1
+		for (i= 0; i<n; i++)
+			v[i]= rand()%n;
+		
+		cerr << "Ejecutando Burbuja para tam. caso: " << n << endl;
+		
+		t0= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que comienza la ejecuciÛn del algoritmo
+		shellSort(v, n); // Ejecutamos el algoritmo para tamaÒo de caso tam
+		tf= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que finaliza la ejecuciÛn del algoritmo
+		
+		unsigned long tejecucion= std::chrono::duration_cast<std::chrono::microseconds>(tf - t0).count();
+		
+		cerr << "\tTiempo de ejec. (us): " << tejecucion << " para tam. caso "<< n<<endl;
+		
+		// Guardamos tam. de caso y t_ejecucion a fichero de salida
+		fsalida<<n<<" "<<tejecucion<<"\n";
+		
+		
+		// Liberamos memoria del vector
+		delete [] v;
+	}
+	
+	// Cerramos fichero de salida
+	fsalida.close();
+	
+	return 0;
+}
+
+
+
