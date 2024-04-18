1f6f9e94990dabd16242afde14e85ff4c4a519a8
albertocmr <cam.ortiz.al@gmail.com>
2024-04-18 20:16:30 +0200

    Añadida a Estructura de Datos la P2 finalizada

---
+++ EstructuradeDatos/Practicas/ej02_pilas_colas/cmake-build-debug/CMakeFiles/maxstack.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/MEGA/Trabajo/Docencia/2022/ED/practicas_ed/solutions_and_proposals/ej02_pilas_colas/estudiante/src/maxstack.cpp" "/mnt/d/MEGA/Trabajo/Docencia/2022/ED/practicas_ed/solutions_and_proposals/ej02_pilas_colas/cmake-build-debug/CMakeFiles/maxstack.dir/estudiante/src/maxstack.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../estudiante/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
