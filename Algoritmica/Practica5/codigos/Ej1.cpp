2d35b02b3cc34d7f477cfa2889d540fd7f7e8288
Alberto Cámara <161840343+albertocmr@users.noreply.github.com>
2024-05-25 19:43:03 +0200

    Update Ej1.cpp

--- Algoritmica/Practica5/codigos/Ej1.cpp
+++ Algoritmica/Practica5/codigos/Ej1.cpp
@@ -1,4 +1,81 @@
-int main(){
+#include <iostream>
+#include <vector>
+#include <limits.h>
 
-    return 0;
+using namespace std;
+
+const int INF = INT_MAX; // Representa el infinito
+
+void floydWarshall(vector<vector<int>>& costes, int tam, int aldeaInicial, int aldeaFinal) {
+  
+  // Inicializar la matriz de distancias con los costes directos
+  vector<vector<int>> C = costes;
+
+  // Algoritmo de Floyd-Warshall
+  for (int k = 0; k < tam; ++k) {
+      for (int i = 0; i < tam; ++i) {
+          for (int j = 0; j < tam; ++j) {
+              if (C[i][k] != INF && C[k][j] != INF) {
+                  C[i][j] = min(C[i][j], C[i][k] + C[k][j]);
+            }
+          }
+      }
+  }
+
+  cout << "Matriz original: " << endl;
+  for (int i = 0; i < tam; ++i) {
+      for (int j = 0; j < tam; ++j) {
+          if (costes[i][j] == INF) {
+              cout << "INF ";
+          } else {
+              cout << costes[i][j] << " ";
+          }
+      }
+      cout << endl;
+  }
+  cout << "-------------------------------------" << endl;
+
+  cout << "Matriz de costes mínimos: " << endl;
+  for (int i = 0; i < tam; ++i) {
+      for (int j = 0; j < tam; ++j) {
+          if (C[i][j] == INF) {
+              cout << "INF ";
+          } else {
+              cout << C[i][j] << " ";
+          }
+      }
+      cout << endl;
+  }
+  cout << "-------------------------------------" << endl;
+  cout << "El coste de ir de la aldea " << aldeaInicial << " a la aldea " << aldeaFinal;
+  cout << " es: " << C[aldeaInicial-1][aldeaFinal-1] << endl;
+
+}
+
+int main(int argc, char * argv[]){
+
+  if(argc != 3){
+    cerr << "Error en el número de argumentos. Uso: " << argv[0] << " <aldea inicial> <aldea final>" << endl;
+    exit(-1);
+  }
+
+  int aldeaInicial = atoi(argv[1]);
+  int aldeaFinal = atoi(argv[2]);
+
+  if(aldeaInicial < 0 || aldeaInicial > 5 || aldeaFinal < 0 || aldeaFinal > 5){
+    cerr << "Error en el rango de las aldeas. Debe ser un valor entre 1 y 5." << endl;
+    exit(-1);
+  }
+
+  vector<vector<int>> costes = {
+      {0, 3, 3, INF, INF},
+      {INF, 0, 4, 7, INF},
+      {INF, INF, 0, 2, 3},
+      {INF, INF, INF, 0, 2},
+      {INF, INF, INF, INF, 0}
+  };
+
+  floydWarshall(costes, costes.size(), aldeaInicial, aldeaFinal);
+
+  return 0;
 }
